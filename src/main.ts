a60202e6e701acb3e402a5a3c4554163202f5978
nicozink <nico@nfzink.com>
2020-06-23 02:05:34 +0100

    Remove submodules and manually sync build_tools

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function run(): Promise<void>
 	{
 		const type: string = core.getInput('type');
 
-		await exec.exec("git", ["submodule", "update", "--init"]);
+		await exec.exec("git", ["clone", "https://github.com/nicozink/build_tools", "libraries/build_tools"]);
 
-		await exec.exec("python", ["build_tools/build_script/configure.py", "--platform", type.toString()]);
+		await exec.exec("python", ["build_tools/build_script/configure.py", "--platform", type.toString(), "."]);
 
 		await exec.exec("cmake --build . --config Release");
 		await exec.exec("ctest -VV -C Release");
