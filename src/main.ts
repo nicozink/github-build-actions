c40e4863b6da9107334bb56c415cc7d55f69f4b0
nicozink <nico@nfzink.com>
2022-02-08 00:45:18 +0000

    Add configure and build steps

--- src/main.ts
+++ src/main.ts
@@ -10,16 +10,28 @@ async function run(): Promise<void>
 		const github_token: string = core.getInput('github_token');
 		const verbose: string = core.getInput("verbose") || "false";
 
+		var config_command = new Array<string>();
+		config_command.push("-B");
+		config_command.push("build");
+		config_command.push("-S");
+		config_command.push(project_root);
+		config_command.push("--config");
+		config_command.push("Release");
+		config_command.push("-DGITHUB_TOKEN=" + github_token.toString());
+
+		await exec.exec("cmake", config_command);
+
 		var build_command = new Array<string>();
 		build_command.push("--build");
 		build_command.push("build");
 		build_command.push("--config");
 		build_command.push("Release");
-		build_command.push("-DGITHUB_TOKEN=" + github_token.toString());
 
 		await exec.exec("cmake", build_command);
 
 		var test_command = new Array<string>();
+		test_command.push("--test-dir");
+		test_command.push("build");
 		test_command.push("-VV");
 		test_command.push("-C");
 		test_command.push("Release");
